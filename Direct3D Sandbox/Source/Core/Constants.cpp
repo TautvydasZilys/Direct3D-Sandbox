31d1495ca6da27abaa44434e993c8959806ca403
Sunius <suniusx@gmail.com>
2014-01-19 19:29:16 +0200

    Enable VSync for Windows Phone - it's not supported otherwise.

--- Direct3D Sandbox/Source/Core/Constants.cpp
+++ Direct3D Sandbox/Source/Core/Constants.cpp
@@ -3,7 +3,12 @@
 
 const wstring Constants::ApplicationName = L"Direct3D Sandbox";
 const bool Constants::ShowCursor = false;
+
+#if !WINDOWS_PHONE
 const bool Constants::VSyncEnabled = false;
+#else
+const bool Constants::VSyncEnabled = true;
+#endif
 
 const int Constants::MultiSampingAntiAliasing = 1;
 const UINT Constants::D3DDeviceFlags = D3D11_CREATE_DEVICE_SINGLETHREADED;
