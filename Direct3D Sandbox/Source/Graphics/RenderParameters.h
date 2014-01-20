064583572e584b77946ae4a8fb87c70597b2f271
Sunius <suniusx@gmail.com>
2014-01-20 23:09:22 +0200

    Use macro to define render parameter fields.

--- Direct3D Sandbox/Source/Graphics/RenderParameters.h
+++ Direct3D Sandbox/Source/Graphics/RenderParameters.h
@@ -2,15 +2,45 @@
 
 #include "PrecompiledHeader.h"
 
+#define GLOBAL_PARAMETERS \
+			FIELD(float, time) \
+			FIELD(DirectX::XMFLOAT4X4, projectionMatrix) \
+			FIELD(DirectX::XMFLOAT4X4, viewMatrix)
+
+
+#define MODEL_PARAMETERS \
+			FIELD(DirectX::XMFLOAT4X4, worldMatrix)
+
+
 struct GlobalParameters
 {
-	float time;
-	DirectX::XMFLOAT4X4 projectionMatrix;
-	DirectX::XMFLOAT4X4 viewMatrix;
+#define FIELD(type, name) type name;
+	GLOBAL_PARAMETERS
+#undef FIELD
+
+	const void* GetField(const string& fieldName) const
+	{
+#define FIELD(type, name) if (fieldName == #name) return &name;
+		GLOBAL_PARAMETERS
+#undef FIELD
+
+		return nullptr;
+	}
 };
 
 struct ModelParameters
-{
-	DirectX::XMFLOAT3 worldMatrix;
+{	
+#define FIELD(type, name) type name;
+	MODEL_PARAMETERS
+#undef FIELD
+
+	const void* GetField(const string& fieldName) const
+	{
+#define FIELD(type, name) if (fieldName == #name) return &name;
+		MODEL_PARAMETERS
+#undef FIELD
+
+		return nullptr;
+	}
 };
 
