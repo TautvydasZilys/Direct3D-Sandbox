c3a033e049dfe88c19032bf58353956047a40f69
Sunius <suniusx@gmail.com>
2014-01-20 23:09:33 +0200

    Implement constant buffers.

--- Direct3D Sandbox/Source/Graphics/ConstantBuffer.cpp
+++ Direct3D Sandbox/Source/Graphics/ConstantBuffer.cpp
@@ -2,22 +2,105 @@
 #include "ConstantBuffer.h"
 #include "Tools.h"
 
-ConstantBuffer::ConstantBuffer(ComPtr<ID3D11ShaderReflectionConstantBuffer> bufferReflection)
+ConstantBuffer::ConstantBuffer(ComPtr<ID3D11Device> device, ID3D11ShaderReflectionConstantBuffer* bufferReflection)
 {
 	HRESULT result;
+	D3D11_BUFFER_DESC constantBufferDescription;
 	D3D11_SHADER_BUFFER_DESC bufferDescription;
-	ComPtr<ID3D11ShaderReflectionVariable> field;
+	D3D11_SHADER_VARIABLE_DESC fieldDescription;
 
 	result = bufferReflection->GetDesc(&bufferDescription);
-	Assert(result);
+	Assert(result = S_OK);
+
+#if DEBUG
+	m_Name = bufferDescription.Name;
+#endif
 
 	for (auto i = 0u; i < bufferDescription.Variables; i++)
 	{
-		field = bufferReflection->GetVariableByIndex(i);
+		auto field = bufferReflection->GetVariableByIndex(i);
 
+		result = field->GetDesc(&fieldDescription);
+		Assert(result == S_OK);
+
+		m_Fields.emplace_back(string(fieldDescription.Name), fieldDescription.StartOffset, fieldDescription.Size);
 	}
+	
+	m_Size = bufferDescription.Size;
+
+	constantBufferDescription.Usage = D3D11_USAGE_DYNAMIC;
+	constantBufferDescription.ByteWidth = m_Size;
+	constantBufferDescription.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	constantBufferDescription.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	constantBufferDescription.MiscFlags = 0;
+	constantBufferDescription.StructureByteStride = 0;
+
+	result = device->CreateBuffer(&constantBufferDescription, nullptr, &m_Buffer);
+	Assert(result == S_OK);
+}
+
+ConstantBuffer::ConstantBuffer(ConstantBuffer&& other) :
+#if DEBUG
+	m_Name(other.m_Name),
+#endif
+	m_Buffer(other.m_Buffer),
+	m_Fields(std::move(other.m_Fields)),
+	m_Size(other.m_Size)
+{
 }
 
 ConstantBuffer::~ConstantBuffer()
 {
+}
+
+void ConstantBuffer::SetRenderParameters(ComPtr<ID3D11DeviceContext> deviceContext, const GlobalParameters& globalParameters, const ModelParameters& modelParameters)
+{
+	bool shouldSet = false;
+
+	for_each (begin(m_Fields), end(m_Fields), [&shouldSet, globalParameters, modelParameters](ConstantBufferField& field)
+	{
+		auto parameterPtr = globalParameters.GetField(field.GetName());		
+		
+		if (parameterPtr == nullptr) 
+		{
+			parameterPtr = modelParameters.GetField(field.GetName());
+		}
+
+		Assert(parameterPtr != nullptr);
+
+		shouldSet |= field.SetValueIfNeeded(parameterPtr);
+	});
+
+	if (shouldSet)
+	{
+		SetRenderParametersNoCheck(deviceContext);
+	}
+}
+
+void ConstantBuffer::SetRenderParametersNoCheck(ComPtr<ID3D11DeviceContext> deviceContext)
+{
+	HRESULT result;
+	D3D11_MAPPED_SUBRESOURCE mappedResource;
+	
+	result = deviceContext->Map(m_Buffer.Get(), 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedResource);
+	Assert(result == S_OK);
+	
+	for_each (begin(m_Fields), end(m_Fields), [&mappedResource](ConstantBufferField& field)
+	{
+		memcpy(reinterpret_cast<uint8_t*>(mappedResource.pData) + field.GetByteOffset(), field.GetValue(), field.GetSize());
+	});
+
+	deviceContext->Unmap(m_Buffer.Get(), 0);
+}
+
+bool ConstantBufferField::SetValueIfNeeded(const void* value)
+{
+	auto shouldSet = memcmp(m_Value.get(), value, m_Size) != 0;
+
+	if (shouldSet)
+	{
+		memcpy(m_Value.get(), value, m_Size);
+	}
+
+	return shouldSet;
 }
--- Direct3D Sandbox/Source/Graphics/ConstantBuffer.h
+++ Direct3D Sandbox/Source/Graphics/ConstantBuffer.h
@@ -1,21 +1,51 @@
 #pragma once
 
+#include "RenderParameters.h"
+
 class ConstantBufferField
 {
-public:
-	ConstantBufferField(int byteOffset, string name) : byteOffset(byteOffset), name(name) {}
+private:
+	string m_Name;
+	unsigned int m_ByteOffset;
+	unsigned int m_Size;
+	unique_ptr<uint8_t[]> m_Value;
 
-	int byteOffset;
-	string name;
+	ConstantBufferField(const ConstantBufferField& other);
+
+public:
+	ConstantBufferField(string name, unsigned int byteOffset, unsigned int size) : 
+		m_Name(std::move(name)), m_ByteOffset(byteOffset), m_Size(size), m_Value(new uint8_t[size]) {}
+
+	ConstantBufferField(ConstantBufferField&& other) : 
+		m_Name(std::move(other.m_Name)), m_ByteOffset(other.m_ByteOffset), m_Size(other.m_Size), m_Value(std::move(other.m_Value)) {}
+
+	inline const string& GetName() const { return m_Name; }
+	inline unsigned int GetByteOffset() const { return m_ByteOffset; }
+	inline unsigned int GetSize() const { return m_Size; }
+	inline const void* GetValue() const { return m_Value.get(); }
+
+	bool SetValueIfNeeded(const void* value);
 };
 
 class ConstantBuffer
 {
 private:
+	ComPtr<ID3D11Buffer> m_Buffer;
 	vector<ConstantBufferField> m_Fields;
+	int m_Size;
 
+#if DEBUG
+	string m_Name;
+#endif
+
+	void SetRenderParametersNoCheck(ComPtr<ID3D11DeviceContext> deviceContext);
+
+	ConstantBuffer(const ConstantBuffer& other);
 public:
-	ConstantBuffer(ComPtr<ID3D11ShaderReflectionConstantBuffer> bufferReflection);
-	~ConstantBuffer();
-};
+	ConstantBuffer(ComPtr<ID3D11Device> device, ID3D11ShaderReflectionConstantBuffer* bufferReflection);
+	ConstantBuffer(ConstantBuffer&& other);
 
+	~ConstantBuffer();
+
+	void SetRenderParameters(ComPtr<ID3D11DeviceContext> deviceContext, const GlobalParameters& globalParameters, const ModelParameters& modelParameters);
+};
