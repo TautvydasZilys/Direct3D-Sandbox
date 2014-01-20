053c11724d10aee6566342eaccfb35fbc96cdfca
Sunius <suniusx@gmail.com>
2014-01-20 23:07:35 +0200

    Implement AutoShader constructor and delete unused fields.

--- Direct3D Sandbox/Source/Graphics/AutoShader.cpp
+++ Direct3D Sandbox/Source/Graphics/AutoShader.cpp
@@ -2,10 +2,16 @@
 #include "AutoShader.h"
 #include "Tools.h"
 
-AutoShader::AutoShader(ComPtr<ID3D11Device> device, wstring vertexShaderPath, wstring pixelShaderPath)
+AutoShader::AutoShader(ComPtr<ID3D11Device> device, wstring vertexShaderPath, wstring pixelShaderPath) :
+	vertexShader(device, vertexShaderPath), pixelShader(device, pixelShaderPath)
 {
 }
 
 AutoShader::~AutoShader()
 {
+}
+
+void AutoShader::Render(const GlobalParameters& globalParameters, const ModelParameters& modelParameters)
+{
+
 }
--- Direct3D Sandbox/Source/Graphics/AutoShader.h
+++ Direct3D Sandbox/Source/Graphics/AutoShader.h
@@ -11,14 +11,6 @@ private:
 	VertexShader vertexShader;
 	PixelShader pixelShader;
 
-	/*ComPtr<ID3D11VertexShader> m_VertexShader;
-	ComPtr<ID3D11PixelShader> m_PixelShader;
-
-	vector<ConstantBuffer> m_VertexShaderConstantBuffers;
-	vector<ConstantBuffer> m_PixelShaderConstantBuffers;*/
-
-	static vector<ConstantBuffer> ReflectConstantBuffers(ComPtr<ID3D11ShaderReflection> shaderReflection, const D3D11_SHADER_DESC& shaderDescription);
-
 public:
 	AutoShader(ComPtr<ID3D11Device> device, wstring vertexShaderPath, wstring pixelShaderPath);
 	virtual ~AutoShader();
