468463af94ee9d9b2c65254f7da0816378c264a9
Sunius <suniusx@gmail.com>
2014-01-23 22:53:18 +0200

    Construct model using model parameters, not render parameters.

--- Direct3D Sandbox/Source/Core/ModelInstance.cpp
+++ Direct3D Sandbox/Source/Core/ModelInstance.cpp
@@ -2,7 +2,7 @@
 #include "ModelInstance.h"
 
 
-ModelInstance::ModelInstance(ComPtr<ID3D11Device> device, IShader& shader, string modelPath, const RenderParameters& renderParameters) :
+ModelInstance::ModelInstance(ComPtr<ID3D11Device> device, IShader& shader, string modelPath, const ModelParameters& renderParameters) :
 	m_Model(Model::Get(modelPath, shader))
 {
 }
