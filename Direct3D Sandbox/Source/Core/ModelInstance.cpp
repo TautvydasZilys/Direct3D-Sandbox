14f2c7fb599003b0ea3c57847b6357899dd25020
Sunius <suniusx@gmail.com>
2014-01-25 19:34:05 +0200

    Implement Model::Render().

--- Direct3D Sandbox/Source/Core/ModelInstance.cpp
+++ Direct3D Sandbox/Source/Core/ModelInstance.cpp
@@ -1,12 +1,25 @@
 #include "PrecompiledHeader.h"
 #include "ModelInstance.h"
-
+#include "Parameters.h"
 
 ModelInstance::ModelInstance(ComPtr<ID3D11Device> device, IShader& shader, string modelPath, const ModelParameters& modelParameters) :
-	m_Model(Model::Get(modelPath, shader))
+	m_Model(Model::Get(modelPath, shader)),
+	m_Parameters(modelParameters)
 {
 }
 
 ModelInstance::~ModelInstance()
 {
+}
+
+void ModelInstance::Render(ComPtr<ID3D11DeviceContext> deviceContext, RenderParameters& renderParameters)
+{
+	DirectX::XMMATRIX scale = DirectX::XMMatrixScaling(m_Parameters.scale.x, m_Parameters.scale.y, m_Parameters.scale.z);
+	DirectX::XMMATRIX rotation = DirectX::XMMatrixRotationRollPitchYaw(m_Parameters.rotation.x, m_Parameters.rotation.y, m_Parameters.rotation.z);
+	DirectX::XMMATRIX position = DirectX::XMMatrixTranslation(m_Parameters.position.x, m_Parameters.position.y, m_Parameters.position.z);
+
+	DirectX::XMStoreFloat4x4(&renderParameters.worldMatrix, DirectX::XMMatrixTranspose(scale * rotation * position));
+	renderParameters.color = m_Parameters.color;
+
+	m_Model.Render(deviceContext, renderParameters);
 }
