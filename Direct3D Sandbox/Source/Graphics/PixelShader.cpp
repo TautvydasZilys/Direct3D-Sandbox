924a0066fafe01b93d6aecbbe537ecef9b24cf87
Sunius <suniusx@gmail.com>
2014-01-26 19:33:22 +0200

    Implement texture and sampler state shader reflection.,

--- Direct3D Sandbox/Source/Graphics/PixelShader.cpp
+++ Direct3D Sandbox/Source/Graphics/PixelShader.cpp
@@ -1,6 +1,7 @@
 #include "PrecompiledHeader.h"
 #include "Parameters.h"
 #include "PixelShader.h"
+#include "SamplerState.h"
 #include "Tools.h"
 
 PixelShader::PixelShader(ComPtr<ID3D11Device> device, wstring path)
@@ -24,4 +25,53 @@ void PixelShader::SetRenderParameters(ComPtr<ID3D11DeviceContext> deviceContext,
 
 	deviceContext->PSSetShader(m_Shader.Get(), nullptr, 0);
 	deviceContext->PSSetConstantBuffers(0, static_cast<UINT>(m_ConstantBufferPtrs.size()), m_ConstantBufferPtrs.data());
+
+	const auto textureCount = m_TextureOffsets.size();
+	unique_ptr<ID3D11ShaderResourceView*[]> textures(new ID3D11ShaderResourceView*[textureCount]);
+
+	for (auto i = 0u; i < textureCount; i++)
+	{
+		memcpy(&textures[i], reinterpret_cast<const uint8_t*>(&renderParameters) + m_TextureOffsets[i], sizeof(ID3D11ShaderResourceView));
+	}
+
+	deviceContext->PSSetShaderResources(0, static_cast<UINT>(textureCount), textures.get());
+	deviceContext->PSSetSamplers(0, static_cast<UINT>(m_SamplerStates.size()), &m_SamplerStates[0]);
+}
+
+void PixelShader::ReflectVirtual(ComPtr<ID3D11Device> device, const vector<uint8_t>& shaderBuffer, ComPtr<ID3D11ShaderReflection> shaderReflection, 
+									const D3D11_SHADER_DESC& shaderDescription)
+{
+	HRESULT result;
+	D3D11_SHADER_INPUT_BIND_DESC desc;
+
+	for (auto i = 0u; i < shaderDescription.BoundResources; i++)
+	{
+		result = shaderReflection->GetResourceBindingDesc(i, &desc);
+		Assert(result == S_OK);
+
+		switch (desc.Type)
+		{
+		case D3D_SIT_TEXTURE:
+			AddTextureOffset(desc.Name);
+			break;
+
+		case D3D_SIT_SAMPLER:
+			AddSamplerState(desc.Name);
+			break;
+		}
+	}
+}
+
+void PixelShader::AddTextureOffset(const string& name)
+{
+	auto offset = RenderParameters::GetFieldByteOffset(name);
+	Assert(offset != 0xFFFFFFFF);
+
+	m_TextureOffsets.push_back(offset);
+}
+
+void PixelShader::AddSamplerState(const string& name)
+{
+	auto samplerState = SamplerState::Get(name);
+	m_SamplerStates.push_back(samplerState);
 }
--- Direct3D Sandbox/Source/Graphics/PixelShader.h
+++ Direct3D Sandbox/Source/Graphics/PixelShader.h
@@ -6,6 +6,15 @@ class PixelShader :
 {
 private:
 	ComPtr<ID3D11PixelShader> m_Shader;
+	vector<unsigned int> m_TextureOffsets;
+	vector<ComPtr<ID3D11SamplerState>> m_SamplerStates;
+
+	void AddTextureOffset(const string& name);
+	void AddSamplerState(const string& name);
+
+protected:	
+	virtual void ReflectVirtual(ComPtr<ID3D11Device> device, const vector<uint8_t>& shaderBuffer, ComPtr<ID3D11ShaderReflection> shaderReflection, 
+		const D3D11_SHADER_DESC& shaderDescription);
 
 public:
 	PixelShader(ComPtr<ID3D11Device> device, wstring path);
@@ -13,4 +22,3 @@ public:
 	
 	virtual void SetRenderParameters(ComPtr<ID3D11DeviceContext> deviceContext, const RenderParameters& renderParameters);
 };
-
