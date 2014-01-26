230973edb5b13eb9306ef386530710d50ad2a7b1
Sunius <suniusx@gmail.com>
2014-01-26 19:32:57 +0200

    Implement static SamplerState class which returns sampler states based on names.

---
+++ Direct3D Sandbox/Source/Graphics/SamplerState.cpp
@@ -0,0 +1,91 @@
+#include "PrecompiledHeader.h"
+#include "SamplerState.h"
+#include "Tools.h"
+
+unordered_map<string, ComPtr<ID3D11SamplerState>> SamplerState::s_SamplerStates;
+
+#if DEBUG
+bool SamplerState::s_Initialized = false;
+#endif
+
+void SamplerState::Initialize(ComPtr<ID3D11Device> device)
+{
+#if DEBUG
+	Assert(s_Initialized == false);
+	s_Initialized = true;
+#endif
+
+	HRESULT result;
+	D3D11_SAMPLER_DESC samplerDescription;
+	ComPtr<ID3D11SamplerState> samplerState;
+
+	samplerDescription.Filter = D3D11_FILTER_ANISOTROPIC;
+	samplerDescription.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
+	samplerDescription.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
+	samplerDescription.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
+	samplerDescription.MipLODBias = 0.0f;
+	samplerDescription.MaxAnisotropy = 16;
+	samplerDescription.ComparisonFunc = D3D11_COMPARISON_ALWAYS;
+	samplerDescription.BorderColor[0] = 0;
+	samplerDescription.BorderColor[1] = 0;
+	samplerDescription.BorderColor[2] = 0;
+	samplerDescription.BorderColor[3] = 0;
+	samplerDescription.MinLOD = 0;
+	samplerDescription.MaxLOD = D3D11_FLOAT32_MAX;
+	
+	result = device->CreateSamplerState(&samplerDescription, &samplerState);
+	Assert(result == S_OK);
+	
+	s_SamplerStates["clampsampler"] = samplerState;
+	
+	samplerDescription.Filter = D3D11_FILTER_ANISOTROPIC;
+	samplerDescription.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDescription.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDescription.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	samplerDescription.MipLODBias = 0.0f;
+	samplerDescription.MaxAnisotropy = 16;
+	samplerDescription.ComparisonFunc = D3D11_COMPARISON_ALWAYS;
+	samplerDescription.BorderColor[0] = 0;
+	samplerDescription.BorderColor[1] = 0;
+	samplerDescription.BorderColor[2] = 0;
+	samplerDescription.BorderColor[3] = 0;
+	samplerDescription.MinLOD = 0;
+	samplerDescription.MaxLOD = D3D11_FLOAT32_MAX;
+	
+	result = device->CreateSamplerState(&samplerDescription, &samplerState);
+	Assert(result == S_OK);
+
+	s_SamplerStates["wrapsampler"] = samplerState;
+
+	samplerDescription.Filter = D3D11_FILTER_COMPARISON_MIN_MAG_LINEAR_MIP_POINT;
+	samplerDescription.AddressU = D3D11_TEXTURE_ADDRESS_BORDER;
+	samplerDescription.AddressV = D3D11_TEXTURE_ADDRESS_BORDER;
+	samplerDescription.AddressW = D3D11_TEXTURE_ADDRESS_BORDER;
+	samplerDescription.MipLODBias = 0.0f;
+	samplerDescription.MaxAnisotropy = 1;
+	samplerDescription.ComparisonFunc = D3D11_COMPARISON_LESS_EQUAL;
+	samplerDescription.BorderColor[0] = 1.0f;
+	samplerDescription.BorderColor[1] = 1.0f;
+	samplerDescription.BorderColor[2] = 1.0f;
+	samplerDescription.BorderColor[3] = 1.0f;
+	samplerDescription.MinLOD = 0;
+	samplerDescription.MaxLOD = D3D11_FLOAT32_MAX;
+	
+	result = device->CreateSamplerState(&samplerDescription, &samplerState);
+	Assert(result == S_OK);
+
+	s_SamplerStates["shadowmapsampler"] = samplerState;
+}
+
+ComPtr<ID3D11SamplerState> SamplerState::Get(const string& name)
+{
+#if DEBUG
+	Assert(s_Initialized == true);
+#endif
+
+	auto samplerName = Tools::ToLower(name);
+	auto sampler = s_SamplerStates.find(samplerName);
+	Assert(sampler != s_SamplerStates.end());
+
+	return sampler->second;
+}
---
+++ Direct3D Sandbox/Source/Graphics/SamplerState.h
@@ -0,0 +1,18 @@
+#pragma once
+
+class SamplerState
+{
+private:
+	static unordered_map<string, ComPtr<ID3D11SamplerState>> s_SamplerStates;
+	
+#if DEBUG
+	static bool s_Initialized;
+#endif
+
+	SamplerState();
+	~SamplerState();
+public:
+	static void Initialize(ComPtr<ID3D11Device> device);
+	static ComPtr<ID3D11SamplerState> Get(const string& name);
+};
+
