314fed96926c07eac6cfdc95b3aacad1de9e0726
Sunius <suniusx@gmail.com>
2014-01-27 20:58:59 +0200

    change main to wWinMain, actually use command line parameters to call reflect shader function.

--- Direct3D Sandbox/Tools/Direct3DPostProcessor/main.cpp
+++ Direct3D Sandbox/Tools/Direct3DPostProcessor/main.cpp
@@ -1,13 +1,27 @@
 #include "PrecompiledHeader.h"
+#include "..\..\Source\Core\Tools.h"
+#include "ShaderReflector.h"
 
-int main(int argc, char** argv)
+int CALLBACK wWinMain(
+  _In_  HINSTANCE hInstance,
+  _In_  HINSTANCE hPrevInstance,
+  _In_  LPWSTR lpCmdLine,
+  _In_  int nCmdShow
+)
 {
+	int argc;
+	wchar_t** argv = CommandLineToArgvW(lpCmdLine, &argc);
+
 	if (argc != 4)
 	{
-		cout << "Invalid number of parameters! Usage: " << argv[0] << "<shaderDirectory> <modelInputDirectory> <modelOutputDirectory>" << endl;
+		wcout << L"Invalid number of parameters! Usage: " << argv[0] << L"<shaderDirectory> <modelInputDirectory> <modelOutputDirectory>" << endl;
 	}
 
+	for (auto& shaderPath : Tools::GetFilesInDirectory(argv[1], L"*.cso", true))
+	{
+		ShaderReflector::ReflectShader(shaderPath);
+	}
 
-
+	LocalFree(argv);
 	return 0;
 }
