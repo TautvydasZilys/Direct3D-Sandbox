22be32e498fa2a668c7a81b055b04df092b7477d
Sunius <suniusx@gmail.com>
2014-02-07 01:16:24 +0200

    Fix texture setting bug.

--- Direct3D Sandbox/Source/Graphics/ShaderProgram.cpp
+++ Direct3D Sandbox/Source/Graphics/ShaderProgram.cpp
@@ -101,22 +101,24 @@ void ShaderProgram::SetConstantBuffers() const
 void ShaderProgram::SetTextures(const RenderParameters& renderParameters)
 {	
 	const auto textureCount = m_TextureOffsets.size();
+	static ShaderProgram* shaderWhichLastSet = nullptr;
 
 	if (textureCount > 0)
 	{
-		bool shouldSet = false;
+		bool textureChanged = false;
 
 		for (auto i = 0u; i < textureCount; i++)
 		{
 			if (memcmp(&m_Textures[i], reinterpret_cast<const uint8_t*>(&renderParameters) + m_TextureOffsets[i], sizeof(ID3D11ShaderResourceView)) != 0)
 			{
-				shouldSet = true;
+				textureChanged = true;
 				memcpy(&m_Textures[i], reinterpret_cast<const uint8_t*>(&renderParameters) + m_TextureOffsets[i], sizeof(ID3D11ShaderResourceView));
 			}
 		}
 
-		if (shouldSet)
+		if (shaderWhichLastSet != this || textureChanged)
 		{
+			shaderWhichLastSet = this;
 			SetTexturesImpl();
 		}
 	}
