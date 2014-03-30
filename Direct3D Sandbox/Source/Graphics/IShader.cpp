ff6e906f187f9e0bcc97909fb9a7cf92ec06f9b1
Sunius <suniusx@gmail.com>
2014-03-30 19:20:14 +0300

    Refactoring.

--- Direct3D Sandbox/Source/Graphics/IShader.cpp
+++ Direct3D Sandbox/Source/Graphics/IShader.cpp
@@ -1,6 +1,7 @@
 #include "PrecompiledHeader.h"
 #include "AutoShader.h"
 #include "IShader.h"
+#include "Tools.h"
 
 vector<shared_ptr<IShader>> IShader::s_Shaders;
 
@@ -14,11 +15,13 @@ IShader::~IShader()
 
 void IShader::LoadShaders()
 {
-	s_Shaders.resize(ShaderType::SHADER_COUNT);
+	s_Shaders.resize(ShaderType::SHADER_COUNT, nullptr);
 	
-	s_Shaders[COLOR_SHADER] = make_shared<AutoShader>(L"Shaders\\ColorVertexShader.cso", L"Shaders\\ColorPixelShader.cso");
-	s_Shaders[TEXTURE_SHADER] = make_shared<AutoShader>(L"Shaders\\TextureVertexShader.cso", L"Shaders\\TexturePixelShader.cso");
-	s_Shaders[NORMAL_MAP_SHADER] = make_shared<AutoShader>(L"Shaders\\NormalMapVertexShader.cso", L"Shaders\\NormalMapPixelShader.cso");
-	s_Shaders[PLAYGROUND_SHADER] = make_shared<AutoShader>(L"Shaders\\PlaygroundVertexShader.cso", L"Shaders\\PlaygroundPixelShader.cso");
-	s_Shaders[INFINITE_GROUND_SHADER] = make_shared<AutoShader>(L"Shaders\\InfiniteGroundVertexShader.cso", L"Shaders\\LightingPixelShader.cso");	
+	s_Shaders[ShaderType::COLOR_SHADER] = make_shared<AutoShader>(L"Shaders\\ColorVertexShader.cso", L"Shaders\\ColorPixelShader.cso");
+	s_Shaders[ShaderType::TEXTURE_SHADER] = make_shared<AutoShader>(L"Shaders\\TextureVertexShader.cso", L"Shaders\\TexturePixelShader.cso");
+	s_Shaders[ShaderType::NORMAL_MAP_SHADER] = make_shared<AutoShader>(L"Shaders\\NormalMapVertexShader.cso", L"Shaders\\NormalMapPixelShader.cso");
+	s_Shaders[ShaderType::PLAYGROUND_SHADER] = make_shared<AutoShader>(L"Shaders\\PlaygroundVertexShader.cso", L"Shaders\\PlaygroundPixelShader.cso");
+	s_Shaders[ShaderType::INFINITE_GROUND_SHADER] = make_shared<AutoShader>(L"Shaders\\InfiniteGroundVertexShader.cso", L"Shaders\\LightingPixelShader.cso");	
+
+	Assert(s_Shaders.size() == ShaderType::SHADER_COUNT);
 }
